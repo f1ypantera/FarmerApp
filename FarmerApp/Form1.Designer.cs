fb319828b0c36c5028cd20e2ea528b35d40c377c
Vadym <flyypaantera@gmail.com>
2021-06-04 23:55:30 +0300

    Updated form

--- FarmerApp/Form1.Designer.cs
+++ FarmerApp/Form1.Designer.cs
@@ -43,10 +43,11 @@ namespace FarmerApp
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-           
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -75,7 +76,7 @@ namespace FarmerApp
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 32;
-            this.listBox1.Location = new System.Drawing.Point(536, 81);
+            this.listBox1.Location = new System.Drawing.Point(1018, 106);
             this.listBox1.Margin = new System.Windows.Forms.Padding(6);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(233, 356);
@@ -83,19 +84,19 @@ namespace FarmerApp
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(536, 488);
+            this.button1.Location = new System.Drawing.Point(1018, 585);
             this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 49);
+            this.button1.Size = new System.Drawing.Size(233, 49);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Clear";
+            this.button1.Text = "Очистити ";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(846, 81);
+            this.comboBox1.Location = new System.Drawing.Point(536, 90);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(242, 40);
             this.comboBox1.TabIndex = 4;
@@ -104,17 +105,50 @@ namespace FarmerApp
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 32;
-            this.listBox2.Location = new System.Drawing.Point(848, 203);
+            this.listBox2.Location = new System.Drawing.Point(536, 250);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(240, 196);
             this.listBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1018, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(354, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Послідовність вирощування";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(536, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(313, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Що будете вирощувати ?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(536, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(296, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ви можете вирощувати";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(1486, 960);
+            this.ClientSize = new System.Drawing.Size(1418, 960);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -123,7 +157,7 @@ namespace FarmerApp
             this.Controls.Add(this.checkedListBox1);
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Додаток - сівозмінна";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +171,9 @@ namespace FarmerApp
         private System.Windows.Forms.Button button1;
         private ComboBox comboBox1;
         private ListBox listBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
 
--- FarmerApp/Form1.cs
+++ FarmerApp/Form1.cs
@@ -17,7 +17,7 @@ namespace FarmerApp
     {
         FurmContext db;
         public List<string> listSt = new List<string>();
-      
+
 
         public Form1()
         {
@@ -39,13 +39,13 @@ namespace FarmerApp
             {
                 // If so, loop through all checked items and print results.  
                 string s = "";
-        
+
                 for (int x = 0; x < checkedListBox1.CheckedItems.Count; x++)
                 {
-                    s = checkedListBox1.CheckedItems[x].ToString();    
+                    s = checkedListBox1.CheckedItems[x].ToString();
                     if (!comboBox1.Items.Contains(s))
                     {
-                    
+
                         comboBox1.Items.Add(s);
                         listSt.Add(s);
                     }
@@ -55,29 +55,27 @@ namespace FarmerApp
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             string selectedState = comboBox1.SelectedItem.ToString();
+            listSt.Remove(selectedState);
+            listBox2.Items.Clear();
             var innerList = new List<string>();
             var test = db.FarmModels.ToList();
             var item = test.FindAll(x => x.Culture == selectedState).Select(x => x.Restrictions).FirstOrDefault();
             string[] subs = item.Split(',');
-            foreach (var t in listSt)
+            foreach (var t in listSt.ToList())
             {
-                if (t != selectedState)
+                if (!subs.Contains(t))
                 {
-                    if (!subs.Contains(t))
-                    {
-                        listBox2.Items.Add(t);
-                        innerList.Add(t);
-                    }
+                    listBox2.Items.Add(t);
+                    innerList.Add(t);
                 }
             }
             listBox1.Items.Add(selectedState);
             comboBox1.Items.Clear();
 
-            foreach(var il in innerList)
+            foreach (var il in innerList)
             {
                 comboBox1.Items.Add(il);
             }
-          
 
         }
 
@@ -87,7 +85,11 @@ namespace FarmerApp
             {
                 checkedListBox1.SetItemCheckState(i, CheckState.Unchecked);
                 listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                comboBox1.Items.Clear();
             }
-        }        
+        }
+
+
     }
 }
